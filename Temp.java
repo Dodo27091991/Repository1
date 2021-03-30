c5f679368b944962f6d3d916899606fd07911817
Sidd <Sid@sid>
2021-03-30 22:40:57 +0530

    updated Temp.java

--- Temp.java
+++ Temp.java
@@ -3,10 +3,10 @@ class Temp
 public static void main(String ar[])
 {
 System.out.println("Hello java");
-
+int i=0;
 for(i=0;i<=10;i++)
 {
-System.out.printly("This is updated");
+System.out.println("This is updated");
 }
 }
 }
