02bd8dde735348fe52b85d992ee622ff12fd3a75
Sidd <Sid@sid>
2021-03-30 23:12:15 +0530

    updated Temp.java

--- Temp.java
+++ Temp.java
@@ -1,12 +1,18 @@
+import java.io.*;
+
 class Temp
 {
 public static void main(String ar[])
 {
 System.out.println("Hello java");
+String s;
 int i=0;
 for(i=0;i<=10;i++)
 {
 System.out.println("This is updated");
+Bufferedreader br=new BufferedReader(new InputStreamReader(System.in))
+s=br.readLine();
+System.out.println(s);
 }
 }
 }
